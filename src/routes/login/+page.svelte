18f8ee0614544520a91155e3b6182613ab5a5da9
Orwellian <orwellian225@gmail.com>
2023-08-07 09:14:07 +0200

    add: Login Page HTML

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+</script>
+
+<div class="w-screen h-screen bg-zinc-100 flex flex-col justify-center items-center gap-10">
+
+    <h1 class="text-5xl font-serif no-underline">Welcome</h1>
+
+    <div class="flex flex-col gap-3">
+        <label for="email">Email</label>
+        <input name="email" type="email" />
+
+        <label for="password">Password</label>
+        <input name="password" type="password" />
+    </div>
+
+    <section class="flex flex-col gap-1 w-1/5">
+        <button class="p-5 bg-emerald-500 rounded-md w-full h-12 text-center text-white text-md flex justify-center items-center hover:bg-emerald-600">Login</button>
+        <button class="p-5 bg-emerald-500 rounded-md w-full h-12 text-center text-white text-md flex justify-center items-center hover:bg-emerald-600">Sign Up</button>
+    </section>
+
+</div>
