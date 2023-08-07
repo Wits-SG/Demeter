71add1cd37eee75b5446eec3ae798f0b787cfe3a
Orwellian <orwellian225@gmail.com>
2023-08-07 10:11:40 +0200

    add: Sign up HTML

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -16,7 +16,7 @@
 
     <section class="flex flex-col gap-1 w-1/5">
         <button class="p-5 bg-emerald-500 rounded-md w-full h-12 text-center text-white text-md flex justify-center items-center hover:bg-emerald-600">Login</button>
-        <button class="p-5 bg-emerald-500 rounded-md w-full h-12 text-center text-white text-md flex justify-center items-center hover:bg-emerald-600">Sign Up</button>
+        <a class="p-5 bg-emerald-500 rounded-md w-full h-12 text-center text-white text-md flex justify-center items-center hover:bg-emerald-600" href="/welcome">Sign Up</a>
     </section>
 
 </div>
