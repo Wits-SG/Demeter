c55b3e489537d2531c4ef7e1e1b3fbd3f8cf4586
Orwellian <orwellian225@gmail.com>
2023-08-13 14:50:35 +0200

    add: designed incomplete signin page

---
+++ src/routes/(signin)/+layout.svelte
@@ -0,0 +1,9 @@
+<div class="bg w-screen h-screen flex flex-row justify-start items-center">
+	<slot />
+</div>
+
+<style>
+	.bg {
+		background-image: url($lib/assets/images/login-background.jpg);
+	}
+</style>
