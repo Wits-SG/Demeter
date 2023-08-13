2556956f7c169dd99db226f2eb278c42309cb64c
nihal-ranchod <nihal.ranchod@gmail.com>
2023-08-13 21:36:02 +0200

    Icons with text

--- src/routes/app/recipe/+page.svelte
+++ src/routes/app/recipe/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import picture from '$lib/assests/pictures/penne-alla-vodka.jpg';
 	import logo from '$lib/assests/pictures/logo.png';
+	import { Icon } from 'flowbite-svelte-icons';
 
 	let recipeName = 'Penna Alla Vodka';
 	let recipeDescription =
 		'A classic Italian-American pasta dish known for its creamy tomato-based sauce with a touch of vodka.';
+	let servingSize = 4;
+	let cookingTime = 30;
+	let skillLevel = 'Intermediate';
 </script>
 
 <div class="w-screen h-screen bg-zinc-100 flex-col items-center justify-center">
@@ -23,9 +27,24 @@
 			<p class="text-lg pt-4">{recipeDescription}</p>
 
 			<div class="grid grid-cols-3 gap-x-40 mt-20">
-				<div>01</div>
-				<div>02</div>
-				<div>03</div>
+				<div class="flex flex-col items-center">
+					<Icon name="users-group-outline" class="h-10 w-10" />
+					<p class="pt-1">
+						{servingSize} Person
+					</p>
+				</div>
+				<div class="flex flex-col items-center">
+					<Icon name="clock-outline" class="h-10 w-10" />
+					<p class="pt-1">
+						~ {cookingTime} Minutes
+					</p>
+				</div>
+				<div class="flex flex-col items-center">
+					<Icon name="arrow-up-down-outline" class="h-10 w-10" />
+					<p class="pt-1">
+						{skillLevel}
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
