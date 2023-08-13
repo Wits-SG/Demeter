b0398bdee8f06af534def1e22fcd92705cb5e8f1
nihal-ranchod <nihal.ranchod@gmail.com>
2023-08-13 15:45:39 +0200

    Fix positioning of logo text

--- src/routes/app/recipe/+page.svelte
+++ src/routes/app/recipe/+page.svelte
@@ -2,17 +2,14 @@
 </script>
 
 <div class="w-screen h-screen bg-zinc-100">
-	<div class="green-bar bg-emerald-500 flex items-center justify-between">
-		<div class="text-zinc-100 text-5xl font-serif text-left">Logo goes here...</div>
+	<div class="green-bar bg-emerald-500 flex items-center justify-start">
+		<div class="text-zinc-100 text-5xl font-serif text-left p-20">Logo goes here...</div>
 	</div>
 </div>
-
-<div class="text-zinc-100 text-5xl font-serif text-left">Logo</div>
 
 <style>
 	.green-bar {
 		height: 130px;
 		width: 100%;
-		padding: 0 100px;
 	}
 </style>
