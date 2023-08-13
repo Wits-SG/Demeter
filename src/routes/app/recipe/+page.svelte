2834222883b273adf9716b753ff107c6960f6d85
nihal-ranchod <nihal.ranchod@gmail.com>
2023-08-13 15:14:07 +0200

    Top bar layout

---
+++ src/routes/app/recipe/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<div class="w-screen h-screen bg-zinc-100">
+	<div class="green-bar bg-emerald-500 flex items-center justify-between">
+		<div class="text-zinc-100 text-5xl font-serif text-left">Logo goes here...</div>
+	</div>
+</div>
+
+<div class="text-zinc-100 text-5xl font-serif text-left">Logo</div>
+
+<style>
+	.green-bar {
+		height: 130px;
+		width: 100%;
+		padding: 0 100px;
+	}
+</style>
