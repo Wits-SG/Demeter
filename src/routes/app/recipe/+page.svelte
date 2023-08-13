c4019002cc1a06c2b4117997ff865906016e0310
nihal-ranchod <nihal.ranchod@gmail.com>
2023-08-13 16:36:47 +0200

    add image

--- src/routes/app/recipe/+page.svelte
+++ src/routes/app/recipe/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+	import picture from '$lib/assets/pictures/penne-alla-vodka.jpg';
 </script>
 
-<div class="w-screen h-screen bg-zinc-100">
+<div class="w-screen h-screen bg-zinc-100f lex flex-col items-center justify-center">
 	<div class="green-bar bg-emerald-500 flex items-center justify-start">
 		<div class="text-zinc-100 text-5xl font-serif text-left p-20">Logo goes here...</div>
 	</div>
+
+	<img class="object-fill" alt="" src={picture} height="300" width="200" />
 </div>
 
 <style>
 	.green-bar {
-		height: 130px;
+		height: 140px;
 		width: 100%;
 	}
 </style>
