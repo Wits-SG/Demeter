8b5fb98fddceab169b18365151427da4adfb5721
nihal-ranchod <nihal.ranchod@gmail.com>
2023-08-13 17:16:27 +0200

    Picture and Position

--- src/routes/app/recipe/+page.svelte
+++ src/routes/app/recipe/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-	import picture from '$lib/assets/pictures/penne-alla-vodka.jpg';
+	import picture from '$lib/assests/pictures/penne-alla-vodka.jpg';
 </script>
 
-<div class="w-screen h-screen bg-zinc-100f lex flex-col items-center justify-center">
+<div class="w-screen h-screen bg-zinc-100 flex-col items-center justify-center">
 	<div class="green-bar bg-emerald-500 flex items-center justify-start">
 		<div class="text-zinc-100 text-5xl font-serif text-left p-20">Logo goes here...</div>
 	</div>
 
-	<img class="object-fill" alt="" src={picture} height="300" width="200" />
+	<div class="flex justify-start items-start p-20 mx-60">
+		<img alt="" src={picture} height="400" width="400" />
+	</div>
 </div>
 
 <style>
