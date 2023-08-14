5584bee9fb852262e4931cffe432b99f085853e6
nihal-ranchod <nihal.ranchod@gmail.com>
2023-08-14 17:13:20 +0200

    Formatting and Boxing

--- src/routes/app/recipe/+page.svelte
+++ src/routes/app/recipe/+page.svelte
@@ -5,7 +5,7 @@
 
 	let recipeName = 'Penne Alla Vodka';
 	let recipeDescription =
-		'A classic Italian-American pasta dish known for its creamy tomato-based sauce with a touch of vodka.';
+		"Penne alla Vodka is a classic Italian-American pasta dish known for its creamy tomato-based sauce with a touch of vodka. This indulgent recipe combines the richness of a velvety sauce with the bite of penne pasta, creating a comforting and flavorful dish that's perfect for a cozy dinner.";
 	let servingSize = 4;
 	let cookingTime = 30;
 	let skillLevel = 'Intermediate';
@@ -31,61 +31,69 @@
 		'Create the Creamy Base: Reduce the heat to low and slowly pour in the heavy cream, stirring constantly to combine. Allow the sauce to gently simmer for about 5-7 minutes until it thickens slightly.',
 		'Combine Pasta and Sauce: Add the cooked penne pasta to the skillet with the sauce. Toss the pasta to coat it evenly with the creamy vodka sauce.',
 		'Finish the Dish: Stir in the grated Parmesan cheese, letting it melt into the sauce and thicken it further. Season with salt and pepper to taste.',
-		'7. Serve: Divide the Penne alla Vodka among serving plates. Garnish with chopped fresh basil or parsley to add a burst of color and freshness.'
+		'Serve: Divide the Penne alla Vodka among serving plates. Garnish with chopped fresh basil or parsley to add a burst of color and freshness.'
 	];
 </script>
 
-<div class="w-screen h-screen bg-zinc-100 flex-col items-center justify-center">
-	<div class="green-bar bg-emerald-500 flex items-center justify-start">
-		<div class="flex justify-start items-start p-20 mx-60">
-			<img alt="" src={logo} height="200" width="200" />
-		</div>
-	</div>
+<div class="w-full h-full bg-zinc-100 flex flex-col justify center gap-20">
+	<div class="green-bar bg-emerald-500 flex items-center justify-start" />
 
-	<div class="bg-zinc-100 flex justify-start items-start p-1">
-		<img alt="" class="mx-40 mt-20" src={picture} height="400" width="400" />
+	<div class="flex flex-row justify start items-start gap-0">
+		<section class="flex flex-col items-center w-1/2">
+			<img alt="" src={picture} height="500" width="400" />
+		</section>
 
-		<div class="flex flex-col justify-between items-center mt-20">
-			<h1 class="text-6xl font-serif p-4">{recipeName}</h1>
-			<h3 class="text-3xl font-serif p-4 underline">Description</h3>
-			<p class="text-lg p-4">{recipeDescription}</p>
+		<div class="flex flex-col items-start gap-10 w-1/2">
+			<section class="flex flex-col items-center gap-10 w-3/4">
+				<h1 class="text-6xl font-serif">{recipeName}</h1>
+				<h3 class="text-3xl font-serif underline">Description</h3>
+				<p class="text-lg">{recipeDescription}</p>
 
-			<div class="border-2 border-slate-950 rounded-lg p-5 mt-6">
-				<div class="grid grid-cols-3 gap-x-40 mt-5">
-					<div class="flex flex-col items-center">
-						<Icon name="users-group-outline" class="h-10 w-10" />
-						<p class="pt-1">
-							{servingSize} Person
-						</p>
+				<!-- *****Using padding need to change***** -->
+				<section class="border-double border-4 border-zinc-800 rounded-lg gap-10 p-4">
+					<div class="grid grid-cols-3 gap-40">
+						<div class="flex flex-col items-center">
+							<Icon name="users-group-outline" class="h-14 w-14" />
+							<p class="">
+								{servingSize} Person
+							</p>
+						</div>
+						<div class="flex flex-col items-center">
+							<Icon name="clock-outline" class="h-14 w-14" />
+							<p class="">
+								~{cookingTime} Minutes
+							</p>
+						</div>
+						<div class="flex flex-col items-center">
+							<Icon name="arrow-up-down-outline" class="h-14 w-14" />
+							<p class="">
+								{skillLevel}
+							</p>
+						</div>
 					</div>
-					<div class="flex flex-col items-center">
-						<Icon name="clock-outline" class="h-10 w-10" />
-						<p class="pt-1">
-							~{cookingTime} Minutes
-						</p>
-					</div>
-					<div class="flex flex-col items-center">
-						<Icon name="arrow-up-down-outline" class="h-10 w-10" />
-						<p class="pt-1">
-							{skillLevel}
-						</p>
-					</div>
-				</div>
-			</div>
+				</section>
+			</section>
 
-			<h3 class="text-3xl font-serif pt-12 underline">Ingredients</h3>
-			<ul class="list-disc list-inside text-lg mt-8">
-				{#each ingredients as ingredient}
-					<li class="p-1">{ingredient}</li>
-				{/each}
-			</ul>
+			<section class="flex flex-col items-center gap-10 w-3/4">
+				<h3 class="text-3xl font-serif underline">Ingredients</h3>
+				<ul class="list-disc list-inside text-lg">
+					{#each ingredients as ingredient}
+						<li class="mb-2">{ingredient}</li>
+					{/each}
+				</ul>
+			</section>
 
-			<h3 class="text-3xl font-serif pt-12 underline">Instructions</h3>
-			<ol class="list-decimal list-inside text-lg mt-10">
-				{#each instructions as instruction}
-					<li class="p-1">{instruction}</li>
-				{/each}
-			</ol>
+			<section class="flex flex-col items-center gap-10 w-3/4">
+				<h3 class="text-3xl font-serif underline">Instructions</h3>
+				<ol class="list-decimal list-inside text-lg">
+					{#each instructions as instruction}
+						<li
+							class="box-content border-zinc-800 shadow-inner shadow-emerald-500 p-4 border-4 mb-6">
+							{instruction}
+						</li>
+					{/each}
+				</ol>
+			</section>
 		</div>
 	</div>
 </div>
