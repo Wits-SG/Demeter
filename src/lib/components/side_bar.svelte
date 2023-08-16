ca32cc3e5e83cf103d4ec04d6fceb501cf2c97a9
Zach SChwark <zachs1708@gmail.com>
2023-08-16 17:19:26 +0200

    final styling

--- src/lib/components/side_bar.svelte
+++ src/lib/components/side_bar.svelte
@@ -1,15 +1,4 @@
 <script lang="ts">
-	import { slide } from 'svelte/transition';
-	import {
-		quintInOut,
-		backInOut,
-		quintOut,
-		quintIn,
-		sineIn,
-		sineOut,
-		backOut,
-		circOut
-	} from 'svelte/easing';
 	let open = false;
 	function slideInOut() {
 		open = !open;
@@ -17,6 +6,7 @@
 </script>
 
 <!-- version with emerald shadow-->
+
 <!--{#if !open}
 	<div class=" flex flex-col bg-zinc-100 dark:bg-zinc-800 w-20 h-full px-4 py-4  shadow-[0_20px_15px_5px_rgba(16,185,129,0.5)]" >
 		<button on:click= {slideInOut} >
@@ -24,7 +14,7 @@
 		</button>
 	</div>
 	{:else}
-		<div class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-80 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(16,185,129,0.5)]">
+		<div class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-60 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(16,185,129,0.5)]">
 			<button on:click= {slideInOut}>
 				Open
 			</button>
@@ -35,6 +25,7 @@
 {/if}-->
 
 <!-- version with emerald border-->
+
 <!--{#if !open}
 <div class=" flex flex-col bg-zinc-100 dark:bg-zinc-800 w-20 h-full px-4 py-4 border-r-2 border-emerald-500" >
 	<button on:click= {slideInOut} >
@@ -42,7 +33,7 @@
 	</button>
 </div>
 {:else}
-	<div class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-80 h-full px-4 py-4 border-r-2 border-emerald-500">
+	<div class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-60 h-full px-4 py-4 border-r-2 border-emerald-500">
 		<button on:click= {slideInOut}>
 			Open
 		</button>
@@ -53,35 +44,35 @@
 {/if} -->
 
 <!-- version with zinc shadow-->
-<!--{#if !open}
-<div class=" flex flex-col bg-zinc-100 dark:bg-zinc-800 w-20 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(113,113,122,0.5)] dark:shadow-[0_20px_15px_5px_rgba(228,228,231,0.5)]" >
-	<button on:click= {slideInOut} >
-	  Open
-	</button>
-</div>
-{:else}
-	<div class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-80 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(113,113,122,0.5)] dark:shadow-[0_20px_15px_5px_rgba(228,228,231,0.5)]">
-		<button on:click= {slideInOut}>
-			Open
-		</button>
-		<button >
-			Home
-		  </button>
+
+{#if !open}
+	<div
+		class=" flex flex-col bg-zinc-100 dark:bg-zinc-800 w-20 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(113,113,122,0.5)] dark:shadow-[0_20px_15px_5px_rgba(228,228,231,0.5)]">
+		<button on:click={slideInOut}> Open </button>
+		<!--Page routing buttons and avatar will go here-->
 	</div>
-{/if}-->
+{:else}
+	<div
+		class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-60 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(113,113,122,0.5)] dark:shadow-[0_20px_15px_5px_rgba(228,228,231,0.5)]">
+		<button on:click={slideInOut}> Open </button>
+		<!--Page routing buttons and avatar will go here-->
+		<button> Home </button>
+	</div>
+{/if}
 
 <!-- version differnt color with zinc shadow-->
-{#if !open}
+
+<!--{#if !open}
 	<div
 		class=" flex flex-col bg-zinc-800 dark:bg-zinc-500 w-20 h-full px-4 py-4 dark:shadow-[0_14px_15px_8px_rgba(212,212,216,0.5)] shadow-[0_14px_15px_5px_rgba(39,39,42,1)]">
 		<button class="text-zinc-100 dark:text-zinc-800" on:click={slideInOut}> Open </button>
-		<!-- Page routing buttons and avatar will go here-->
+		Page routing buttons and avatar will go here
 	</div>
 {:else}
 	<div
-		class="flex flex-col bg-zinc-800 dark:bg-zinc-500 w-80 h-full px-4 py-4 dark:shadow-[0_14px_15px_8px_rgba(212,212,216,0.5)] shadow-[0_14px_15px_5px_rgba(39,39,42,1)]">
+		class="flex flex-col bg-zinc-800 dark:bg-zinc-500 w-60 h-full px-4 py-4 dark:shadow-[0_14px_15px_8px_rgba(212,212,216,0.5)] shadow-[0_14px_15px_5px_rgba(39,39,42,1)]">
 		<button class="text-zinc-100 dark:text-zinc-800" on:click={slideInOut}> Open </button>
-		<!-- Page routing buttons and avatar will go here-->
+		Page routing buttons and avatar will go here
 		<button class="text-zinc-100 dark:text-zinc-800"> Home </button>
 	</div>
-{/if}
+{/if}-->
--- src/lib/components/top_bar.svelte
+++ src/lib/components/top_bar.svelte
@@ -1,20 +1,22 @@
 <script>
 	import ZinclogoDark from '$lib/assets/images/Demeter_logo_Zinc_dark.png';
+	import ZincLogoLight from '$lib/assets/images/Demeter_logo_Zinc_light (1).png';
 </script>
 
-<!--<div
+<div
 	class="bg-zinc-100 dark:bg-zinc-800 w-full h-20 px-10 py-2 border-b-2 border-emerald-500 flex flex-row justify-between items-center">
-	<img src={EmeraldlogoDark} class="h-auto w-16" alt="logo" />
+	<img src={ZincLogoLight} class="h-auto w-16" alt="logo" />
 
 	<input
 		class="p-1 w-64 text-black dark:text-zinc-100 bg-zinc-100 dark:bg-zinc-800 rounded-md outline outline-1 outline-zinc-300 focuse:outline-0 focus:outline-2 focus:outline-emerald-500"
 		placeholder="Search"
 		type="search"
 		name="search" />
-</div>-->
+</div>
 
 <!-- Version with complete solid color-->
-<div
+
+<!--<div
 	class="bg-zinc-800 dark:bg-zinc-500 w-full h-20 px-10 py-2 border-b-2 border-emerald-500 flex flex-row justify-between items-center">
 	<img src={ZinclogoDark} class="h-auto w-16" alt="logo" />
 
@@ -23,9 +25,10 @@
 		placeholder="Search"
 		type="search"
 		name="search" />
-</div>
+</div>-->
 
 <!-- Version with complete solid color and no green border-->
+
 <!--<div
 	class="bg-zinc-800 dark:bg-zinc-500 w-full h-20 px-10 py-2 flex flex-row justify-between items-center">
 	<img src={ZinclogoDark} class="h-auto w-16" alt="logo" />
