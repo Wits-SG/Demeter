c99beed2ef621b829de58e3f56d294c759618973
Orwellian <orwellian225@gmail.com>
2023-08-16 19:50:49 +0200

    fix: nav layout and dark mode on recipe page

--- src/routes/app/recipe/+page.svelte
+++ src/routes/app/recipe/+page.svelte
@@ -46,7 +46,7 @@
 	}
 </script>
 
-<div class="w-full h-full bg-zinc-100 flex flex-col justify center items-center gap-20">
+<div class=" bg-zinc-100 dark:bg-zinc-800 flex flex-col justify-center items-center gap-20">
 	<div class="flex flex-row justify start items-start gap-0">
 		<section class="flex flex-col items-center w-1/2 gap-5">
 			<section class="container md:mx-auto w-2/3">
@@ -57,7 +57,7 @@
 				<div class="flex flex-col items-center gap-2">
 					<Icon name="message-caption-outline" class="h-14 w-14" />
 					<button
-						class="p-5 bg-emerald-500 rounded-full w-full h-12 text-center text-white text-md flex justify-center items-center hover:bg-emerald-600">
+						class="p-5 bg-emerald-500 rounded-full w-full h-12 text-center text-md flex justify-center items-center hover:bg-emerald-600">
 						Write a review
 					</button>
 				</div>
@@ -65,21 +65,22 @@
 					<Icon name="heart-outline" class="h-14 w-14" />
 					<button
 						on:click={toggleSavedState}
-						class="p-5 bg-emerald-500 rounded-full w-full h-12 text-center text-white text-md flex justify-center items-center hover:bg-emerald-600">
+						class="p-5 bg-emerald-500 rounded-full w-full h-12 text-center text-md flex justify-center items-center hover:bg-emerald-600">
 						{savedState}
 					</button>
 				</div>
 			</section>
 		</section>
 
-		<div class="flex flex-col items-start gap-10 w-1/2">
+		<div class="flex flex-col items-start gap-10 w-1/2 h-full">
 			<section class="flex flex-col items-center gap-10 w-3/4">
 				<h1 class="text-6xl font-serif">{recipeName}</h1>
 				<h3 class="text-3xl font-serif underline">Description</h3>
 				<p class="text-lg">{recipeDescription}</p>
 
 				<!-- *****Using padding need to change***** -->
-				<section class="border-double border-4 border-zinc-800 rounded-lg gap-10 p-2">
+				<section
+					class="border-double border-4 border-zinc-800 dark:border-zinc-100 rounded-lg gap-10 p-2">
 					<div class="grid grid-cols-3 items-center justify-center gap-40">
 						<div class="flex flex-col items-center justify-center">
 							<Icon name="users-group-outline" class="h-14 w-14" />
