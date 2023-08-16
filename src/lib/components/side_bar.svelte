f401e6e51fcb3b3edb442615e473a04212fd2437
Zach SChwark <zachs1708@gmail.com>
2023-08-16 17:34:20 +0200

    code clean up

--- src/lib/components/side_bar.svelte
+++ src/lib/components/side_bar.svelte
@@ -5,46 +5,7 @@
 	}
 </script>
 
-<!-- version with emerald shadow-->
-
-<!--{#if !open}
-	<div class=" flex flex-col bg-zinc-100 dark:bg-zinc-800 w-20 h-full px-4 py-4  shadow-[0_20px_15px_5px_rgba(16,185,129,0.5)]" >
-		<button on:click= {slideInOut} >
-		  Open
-		</button>
-	</div>
-	{:else}
-		<div class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-60 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(16,185,129,0.5)]">
-			<button on:click= {slideInOut}>
-				Open
-			</button>
-			<button >
-				Home
-		  	</button>
-		</div>
-{/if}-->
-
-<!-- version with emerald border-->
-
-<!--{#if !open}
-<div class=" flex flex-col bg-zinc-100 dark:bg-zinc-800 w-20 h-full px-4 py-4 border-r-2 border-emerald-500" >
-	<button on:click= {slideInOut} >
-	  Open
-	</button>
-</div>
-{:else}
-	<div class="flex flex-col bg-zinc-100 dark:bg-zinc-800 w-60 h-full px-4 py-4 border-r-2 border-emerald-500">
-		<button on:click= {slideInOut}>
-			Open
-		</button>
-		<button >
-			Home
-		  </button>
-	</div>
-{/if} -->
-
 <!-- version with zinc shadow-->
-
 {#if !open}
 	<div
 		class=" flex flex-col bg-zinc-100 dark:bg-zinc-800 w-20 h-full px-4 py-4 shadow-[0_20px_15px_5px_rgba(113,113,122,0.5)] dark:shadow-[0_20px_15px_5px_rgba(228,228,231,0.5)]">
@@ -59,20 +20,3 @@
 		<button> Home </button>
 	</div>
 {/if}
-
-<!-- version differnt color with zinc shadow-->
-
-<!--{#if !open}
-	<div
-		class=" flex flex-col bg-zinc-800 dark:bg-zinc-500 w-20 h-full px-4 py-4 dark:shadow-[0_14px_15px_8px_rgba(212,212,216,0.5)] shadow-[0_14px_15px_5px_rgba(39,39,42,1)]">
-		<button class="text-zinc-100 dark:text-zinc-800" on:click={slideInOut}> Open </button>
-		Page routing buttons and avatar will go here
-	</div>
-{:else}
-	<div
-		class="flex flex-col bg-zinc-800 dark:bg-zinc-500 w-60 h-full px-4 py-4 dark:shadow-[0_14px_15px_8px_rgba(212,212,216,0.5)] shadow-[0_14px_15px_5px_rgba(39,39,42,1)]">
-		<button class="text-zinc-100 dark:text-zinc-800" on:click={slideInOut}> Open </button>
-		Page routing buttons and avatar will go here
-		<button class="text-zinc-100 dark:text-zinc-800"> Home </button>
-	</div>
-{/if}-->
--- src/lib/components/top_bar.svelte
+++ src/lib/components/top_bar.svelte
@@ -13,29 +13,3 @@
 		type="search"
 		name="search" />
 </div>
-
-<!-- Version with complete solid color-->
-
-<!--<div
-	class="bg-zinc-800 dark:bg-zinc-500 w-full h-20 px-10 py-2 border-b-2 border-emerald-500 flex flex-row justify-between items-center">
-	<img src={ZinclogoDark} class="h-auto w-16" alt="logo" />
-
-	<input
-		class="p-1 w-64 text-black dark:text-zinc-100 bg-zinc-800 dark:bg-zinc-100 rounded-md outline outline-1 outline-zinc-300 focuse:outline-0 focus:outline-2 focus:outline-emerald-500"
-		placeholder="Search"
-		type="search"
-		name="search" />
-</div>-->
-
-<!-- Version with complete solid color and no green border-->
-
-<!--<div
-	class="bg-zinc-800 dark:bg-zinc-500 w-full h-20 px-10 py-2 flex flex-row justify-between items-center">
-	<img src={ZinclogoDark} class="h-auto w-16" alt="logo" />
-
-	<input
-		class="p-1 w-64 text-black dark:text-zinc-100 bg-zinc-800 dark:bg-zinc-100 rounded-md outline outline-1 outline-zinc-300 focuse:outline-0 focus:outline-2 focus:outline-emerald-500"
-		placeholder="Search"
-		type="search"
-		name="search" />
-</div>-->
