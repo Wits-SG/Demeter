27a506ec7e1905f9c0c6bd5e6c1f25273c48f869
Ashlea <smithashlea@gmail.com>
2023-08-17 21:44:03 +0200

    Cookbook page UI
    Add: basic cookbook page UI

---
+++ src/routes/app/cookbook/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex flex-col mx-20 h-screen w-screen">
+	<div class="mx-60 mb-20 flex flex-col">
+		<h1 class="text-3xl">COOKBOOKS</h1>
+		<div class="mx-10 mt-20 space-x-10 flex flex-row">
+			<button class=" text-2xl px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">+</button>
+			<button class="px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">Book</button>
+		</div>
+	</div>
+	<hr class="h-1 my-4 md:my-10 mx-40 bg-emerald-700 dark:bg-emerald-300 border-0" />
+
+	<div class="mx-60 mbt-20 flex flex-col">
+		<div class="flex flex-col">
+			<h1 class="text-3xl">MENUS</h1>
+			<div class="mx-10 mt-20 space-x-10 flex flex-row">
+				<button class=" text-2xl px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">+</button>
+				<button class="px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">Menu</button>
+			</div>
+		</div>
+	</div>
+</div>
