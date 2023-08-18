bec7e4afb594ca408bbb9e2e35363ceb57bcf464
Orwellian <orwellian225@gmail.com>
2023-08-18 08:32:40 +0200

    remove: old base url page

--- src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<h1 class="text-3xl font-bold underline font-serif">Welcome to SvelteKit</h1>
-<p class="text-m font-sans underline-offset-1">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
