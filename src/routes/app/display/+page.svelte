d26392bdae2de82b508b35336c7b35d287ac8aac
Ashlea <smithashlea@gmail.com>
2023-08-18 14:08:01 +0200

    Cookbook Display
    Add: Basic Layout of cookbook display

---
+++ src/routes/app/display/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex flex-row h-full w-full justify-center gap-20">
+	<div class=" mt-24 flex flex-col border-4 border-emerald-700 h-3/4 w-1/6 text-center">
+		<h2 class="text-xl font-semibold text-emerald-700 mt-5">INDEX</h2>
+	</div>
+
+	<div
+		class="mt-24 flex flex-col border-4 border-emerald-700 h-3/4 w-2/3 text-center items-stretch">
+		<div class="mt-20 self-center flex flex-col border-2 border-emerald-700 h-5/6 w-11/12">
+			Small Recipe
+		</div>
+		<div class="mt-5 flex flex-row space-x-96 h-1/9 w-11/12 justify-between">
+			<button
+				class="ml-20 rounded-md border-2 w-20 h-10 border-emerald-700 text-emerald-700 text-lg font-semibold"
+				>Prev</button>
+			<button
+				class="mr-2 rounded-md border-2 w-20 h-10 border-emerald-700 text-emerald-700 text-lg font-semibold"
+				>Next</button>
+		</div>
+	</div>
+</div>
