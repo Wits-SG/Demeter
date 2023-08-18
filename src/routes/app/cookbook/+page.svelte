1113d471f3cc3a0d60a050bed77c9d3544cda067
Ashlea <smithashlea@gmail.com>
2023-08-18 11:22:24 +0200

    Changed Main cookbook page
    Add: better looking UI

--- src/routes/app/cookbook/+page.svelte
+++ src/routes/app/cookbook/+page.svelte
@@ -1,19 +1,27 @@
-<div class="flex flex-col mx-20 h-screen w-screen">
-	<div class="mx-60 mb-20 flex flex-col">
+<div class="mt-20 flex flex-col mx-20 h-screen w-screen">
+	<div class="flex flex-col">
 		<h1 class="text-3xl">COOKBOOKS</h1>
-		<div class="mx-10 mt-20 space-x-10 flex flex-row">
-			<button class=" text-2xl px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">+</button>
-			<button class="px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">Book</button>
+		<div class="mt-10 ml-10 space-x-10 flex flex-row align-center">
+			<button
+				class="rounded-md border-2 w-32 h-32 border-emerald-700 text-4xl text-emerald-700 dark:text-emerald-300 dark:border-emerald-300"
+				>+</button>
+			<button
+				class="rounded-md border-2 w-32 h-32 border-emerald-700 text-xl text-emerald-700 dark:text-emerald-300 dark:border-emerald-300"
+				>RECIPE BOOK</button>
 		</div>
 	</div>
-	<hr class="h-1 my-4 md:my-10 mx-40 bg-emerald-700 dark:bg-emerald-300 border-0" />
+	<hr class="h-1 my-4 md:my-10 bg-emerald-700 dark:bg-emerald-300 border-0" />
 
-	<div class="mx-60 mbt-20 flex flex-col">
+	<div class="flex flex-col">
 		<div class="flex flex-col">
 			<h1 class="text-3xl">MENUS</h1>
-			<div class="mx-10 mt-20 space-x-10 flex flex-row">
-				<button class=" text-2xl px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">+</button>
-				<button class="px-9 py-3.5 bg-emerald-700 dark:bg-emerald-300">Menu</button>
+			<div class="mt-10 ml-10 space-x-10 flex flex-row align-center">
+				<button
+					class="rounded-md border-2 w-32 h-32 border-emerald-700 text-4xl text-emerald-700 dark:text-emerald-300 dark:border-emerald-300"
+					>+</button>
+				<button
+					class="rounded-md border-2 w-32 h-32 border-emerald-700 text-xl text-emerald-700 dark:text-emerald-300 dark:border-emerald-300"
+					>Saturday Menu</button>
 			</div>
 		</div>
 	</div>
