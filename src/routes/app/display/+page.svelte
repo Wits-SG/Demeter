2fda4e51fc137579bde38d63153dd3f3092b3b3d
Ashlea <smithashlea@gmail.com>
2023-08-21 11:15:23 +0200

    UI updates

--- src/routes/app/display/+page.svelte
+++ src/routes/app/display/+page.svelte
@@ -4,11 +4,11 @@
 	</div>
 
 	<div
-		class="mt-24 flex flex-col border-4 border-emerald-700 h-3/4 w-2/3 text-center items-stretch">
+		class=" mt-24 flex flex-col border-4 border-emerald-700 h-3/4 w-2/3 text-center items-stretch">
 		<div class="mt-20 self-center flex flex-col border-2 border-emerald-700 h-5/6 w-11/12">
 			Small Recipe
 		</div>
-		<div class="mt-5 flex flex-row space-x-96 h-1/9 w-11/12 justify-between">
+		<div class=" flex flex-row space-x-96 h-1/9 w-11/12 justify-between">
 			<button
 				class="ml-20 rounded-md border-2 w-20 h-10 border-emerald-700 text-emerald-700 text-lg font-semibold"
 				>Prev</button>
