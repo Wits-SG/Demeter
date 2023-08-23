72ae0efa959209a96a43612522c21ac0d12ea2ec
Zach SChwark <zachs1708@gmail.com>
2023-08-23 12:03:52 +0200

    link backend, clean up code

--- src/lib/components/recipe_preview.svelte
+++ src/lib/components/recipe_preview.svelte
@@ -1,35 +1,33 @@
 <script lang="ts">
-	import dishPicture from '$lib/assets/images/pavs.jpg';
-	import { Icon } from 'flowbite-svelte-icons';
 	import { onMount } from 'svelte';
-	import { get } from 'svelte/store';
-	export let recipeID: Number = 1;
+	export let recipeID: Number;
+	let recipePreviewData: any;
 
 	const getPreviewData = async () => {
 		try {
-			const recipePreview_res = await fetch('src/routes/api/recipe_preview', {
-				method: 'POST',
-				body: JSON.stringify({
-					recipeID: 1
-				}),
-				headers: {
-					'content-type': 'application/json'
-				}
+			const recipePreview_res = await fetch(`/api/recipe_preview?recipe_id=${recipeID}`, {
+				method: 'GET'
 			});
-			console.log(recipePreview_res.json);
+
+			const recipe_data: JSON = await recipePreview_res.json();
+			console.log(recipe_data);
+			return recipe_data;
 		} catch (recipePreview_err: any) {
 			console.log('error occured in fetch');
 		}
 	};
 
-	onMount(() => {
-		//fetch data using recipeID here;
-		getPreviewData();
-	});
+	let dishTitle = 'NONE';
+	let dishImageURL = 'NONE';
+	let dishDescription = 'NONE';
 
-	let dishTitle = 'Penne Alla Vodka';
-	let dishDescription =
-		"Penne alla Vodka is a classic Italian-American pasta dish known for its creamy tomato-based sauce with a touch of vodka. This indulgent recipe combines the richness of a velvety sauce with the bite of penne pasta, creating a comforting and flavorful dish that's perfect for a cozy dinner.";
+	onMount(async () => {
+		//fetch data using recipeID here;
+		recipePreviewData = await getPreviewData();
+		dishTitle = recipePreviewData.recipe.name;
+		dishDescription = recipePreviewData.recipe.description;
+		dishImageURL = recipePreviewData.recipe.imageURL;
+	});
 </script>
 
 <a href="/recipe/{recipeID}">
@@ -38,27 +36,13 @@
 		<div class="md:flex md:flex-col">
 			<img
 				class="h-28 w-full md:h-48 md:w-full object-cover md:object-cover"
-				src={dishPicture}
+				src={dishImageURL}
 				alt="Dish" />
 			<div class="p-4">
 				<div class="mb-2 uppercase text-sm text-zinc-800 dark:text-zinc-100 font-semibold">
 					{dishTitle}
 					<p>{recipeID}</p>
 				</div>
-				<!--<div class="m-1 grid grid-cols-3 items-center justify-center gap-0.25">
-				<div class="flex flex-col items-center justify-center gap-1">
-					<Icon name="users-group-outline" class="h-3 w-3" />
-					<p class="text-xs">4 Person</p>
-				</div>
-				<div class="flex flex-col items-center justify center gap-1">
-					<Icon name="clock-outline" class="h-3 w-3" />
-					<p class="text-xs">~30 Mins</p>
-				</div>
-				<div class="flex flex-col items-center justify center gap-1">
-					<Icon name="arrow-up-down-outline" class="h-3 w-3" />
-					<p class="text-xs">Intermediate</p>
-				</div>
-			</div>-->
 				<p class="line-clamp-3 mt-2 text-zinc-500 dark:text-zinc-300 text-sm">
 					{dishDescription}
 				</p>
--- src/routes/(app)/Feed/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import RecipePreview from '$lib/components/recipe_preview.svelte';
-	let recipeID: Number = 1;
-</script>
-
-<div class=" h-full w-full p-10">
-	<RecipePreview {recipeID} />
-</div>
