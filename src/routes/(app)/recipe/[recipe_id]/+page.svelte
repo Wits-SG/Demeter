ab8d083499b50995a3a0415369863dc53167f8c6
nihal-ranchod <nihal.ranchod@gmail.com>
2023-08-28 22:17:53 +0200

    UI Changes: Light & dark mode

--- src/routes/(app)/recipe/[recipe_id]/+page.svelte
+++ src/routes/(app)/recipe/[recipe_id]/+page.svelte
@@ -55,10 +55,6 @@
 
 <div
 	class="w-full h-full bg-zinc-100 dark:bg-zinc-800 flex flex-col justify-center items-center gap-20 pt-6">
-	<div class="flex flex-col justify-start items-start">
-		<h1 class="text-6xl font-serif">{data.recipe.name}</h1>
-	</div>
-
 	<div class="flex flex-row justify start items-start gap-1">
 		<section class="flex flex-col items-center justify-center w-1/2 gap-10">
 			<section class="container md:mx-auto w-2/3">
@@ -119,6 +115,7 @@
 
 		<div class="flex flex-col items-start gap-20 w-1/2">
 			<section class="flex flex-col items-center gap-10 w-4/5">
+				<h1 class="text-6xl font-serif sticky top-0">{data.recipe.name}</h1>
 				<h3 class="text-3xl font-serif underline">Description</h3>
 				<p class="text-lg">{data.recipe.description}</p>
 			</section>
@@ -133,7 +130,7 @@
 								<button
 									use:melt={$trigger(id)}
 									class="flex flex-1 cursor-pointer items-center justify-center
-										bg-teal-600 px-5 py-5 text-lg leading-none
+										bg-teal-600 px-7 py-7 text-lg leading-none
 										text-white transition-colors hover:bg-emerald-300 focus:!ring-0
 										focus-visible:text-magnum-800
 										i !== 0 && 'border-t border-t-neutral-300">
@@ -142,17 +139,16 @@
 							</h2>
 							{#if $isSelected(id)}
 								<div
-									class="content overflow-hidden bg-neutral-100 text-md text-neutral-600"
+									class="content overflow-hidden bg-zinc-200 text-md text-zinc-950 dark:bg-zinc-600 dark:text-zinc-100"
 									use:melt={$content(id)}
 									transition:slide>
 									<div class="px-5 py-4">
 										{#if title == 'INSTRUCTIONS'}
 											<div>
-												<ol
-													class="list-decimal list-inside text-lg overflow-y-scroll">
+												<ol class="list-decimal list-inside text-lg">
 													{#each data.recipe.instructions as instruction}
 														<li
-															class="box-content hover:bg-zinc-300 dark:hover:bg-zinc-200 p-3 border-4 mb-6">
+															class="box-content text-zinc-950 bg-gray-50 hover:bg-blue-50 dark:hover:bg-zinc-400 dark:bg-zinc-800 dark:text-zinc-100 p-3 border-2 mb-6 border-teal-500 rounded-lg">
 															{instruction}
 														</li>
 													{/each}
@@ -165,7 +161,7 @@
 														class="flex flex-row justify-start items-center mb-2 gap-4">
 														<Icon
 															name="check-circle-outline"
-															class="h-4 w-4 text-teal-400" />{ingredient}
+															class="h-4 w-4 text-green-500" />{ingredient}
 													</li>
 												{/each}
 											</ul>
