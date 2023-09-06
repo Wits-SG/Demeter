c8b9044a882148ec6227c473b0065fd2c2bf3f81
Orwellian <orwellian225@gmail.com>
2023-09-06 12:27:59 +0200

    add: Main Page Navigation

---
+++ src/lib/components/navigation/page_nav.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { createDropdownMenu, melt } from '@melt-ui/svelte';
+	import { goto } from '$app/navigation';
+	import { fly } from 'svelte/transition';
+	import { Icon } from 'flowbite-svelte-icons';
+
+	interface NavPage {
+		name: string;
+		href: string;
+		iconName: string;
+	}
+
+	const navPages: Array<NavPage> = [
+		{ name: 'Home', href: '/', iconName: 'home-outline' },
+		{ name: 'Explore', href: '/', iconName: 'papper-plane-outline' },
+		{ name: 'Cookbook', href: '/', iconName: 'book-outline' }
+	];
+	let currentNavPage: NavPage = navPages[0];
+
+	const {
+		elements: { trigger, menu, item },
+		states: { open }
+	} = createDropdownMenu({ forceVisible: true });
+</script>
+
+<button
+	use:melt={$trigger}
+	class="h-8 w-64 min-w-fit rounded-lg bg-zinc-200 flex justify-center items-center gap-4 text-lg"
+	>{currentNavPage.name}</button>
+{#if $open}
+	<div
+		class="bg-zinc-200 h-fit w-64 dark:text-black p-2 flex flex-col justify-center items-start rounded-lg gap-3"
+		use:melt={$menu}
+		transition:fly={{ duration: 150, y: -10 }}>
+		{#each navPages as navPage}
+			<button
+				class="flex flex-row justify-center items-center gap-5"
+				use:melt={$item}
+				on:click={() => {
+					currentNavPage = navPage;
+					goto(navPage.href);
+				}}><Icon name={navPage.iconName} class="h-6 w-6" /> {navPage.name}</button>
+		{/each}
+	</div>
+{/if}
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,60 +1,31 @@
 <script lang="ts">
-	import TopBar from '$lib/components/top_bar.svelte';
-	import { createCombobox, melt, type ComboboxFilterFunction } from '@melt-ui/svelte';
 	import { Icon } from 'flowbite-svelte-icons';
-
-	interface NavPage {
-		displayName: string;
-		href: string;
-	}
-
-	let navPages: Array<NavPage> = [
-		{ displayName: 'Home', href: '/' },
-		{ displayName: 'Explore', href: '/explore' },
-		{ displayName: 'Cookbook', href: '/cookbook' }
-	];
-
-	const filterFunction: ComboboxFilterFunction<NavPage> = ({ itemValue, input }) => {
-		return true;
-	};
-
-	const {
-		elements: { input, item, menu },
-		states: { open },
-		helpers: { isSelected }
-	} = createCombobox({
-		filterFunction,
-		forceVisible: true,
-		items: navPages
-	});
+	import PageNav from '$lib/components/navigation/page_nav.svelte';
 </script>
 
 <main class="grid grid-cols-1 grid-rows-[90px_1fr] h-full w-full overflow-x-hidden">
 	<div
 		class="row-start-1 row-span-1 sticky top-0 w-full h-full flex justify-center items-center bg-zinc-100 dark:bg-zinc-800 backdrop-blur-3xl">
 		<nav
-			class="h-full w-full flex flex-row items-center gap-2 justify-center rounded-b-md bg-zinc-500 px-5 backdrop-blur-md backdrop-opacity-5">
-			<div class="h-2/3 w-20 rounded-lg bg-emerald-500 flex justify-center items-center">
-				<Icon name="swatchbook-outline" class="p-2 w-full h-full text-zinc-500" />
-			</div>
+			class="h-full w-full flex flex-row items-center gap-2 justify-between rounded-b-md bg-zinc-500 px-5 backdrop-blur-md backdrop-opacity-5 dark:text-black">
+			<section class="h-full w-full flex flex-row justify-start items-center gap-5">
+				<div
+					class="h-2/3 w-20 rounded-lg bg-emerald-500 flex justify-center items-center" />
+				<PageNav />
+				<button class="p-1 h-fit w-fit bg-zinc-200 rounded-lg"
+					><Icon name="plus-outline" class="h-6 w-6 " /></button>
+			</section>
 
-			<input
-				use:melt={$input}
-				class="flex h-1/3 w-64 min-w-fit max-w-full items-center justify-center gap-0 rounded-md bg-zinc-300 hover:bg-zinc-200" />
-			{#if $open}
-				<ul use:melt={$menu}>
-					{#each navPages as navPage}
-						<li
-							use:melt={$item({
-								value: navPage.href,
-								label: navPage.displayName,
-								disabled: true
-							})}>
-							{navPage.displayName}
-						</li>
-					{/each}
-				</ul>
-			{/if}
+			<section class="h-full w-full flex flex-row justify-end items-center gap-5">
+				<a href="/settings" class="p-1 h-fit w-fit bg-zinc-200 rounded-lg"
+					><Icon name="cog-outline" class="h-6 w-6" /></a>
+				<input
+					class="h-8 w-64 min-w-fit rounded-lg bg-zinc-200 flex justify-center items-center p-1 outline-none focus:outline-2 focus:outline-emerald-500"
+					type="search"
+					placeholder="search" />
+				<div
+					class="h-16 w-16 rounded-full bg-emerald-700 flex justify-center items-center" />
+			</section>
 		</nav>
 	</div>
 	<div class="row-start-2 row-span-1 col-span-1"><slot /></div>
