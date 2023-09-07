ee7d4b82b4235d879e0d6071292b88e7397cb8ff
Zach SChwark <zachs1708@gmail.com>
2023-09-07 14:41:54 +0200

    finishing touches on mosaic

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,39 +2,50 @@
 	import type { PageData } from './$types';
 	import RecipePreview from '$lib/components/recipe_preview.svelte';
 	import Masonry from 'svelte-bricks';
-	import { Toggle } from 'flowbite-svelte';
+	import { Icon } from 'flowbite-svelte-icons';
 
 	export let data: PageData;
 	let items = data.recipes;
-	let [minColWidth, maxColWidth, gap] = [280, 350, 20];
+	let [minColWidth, maxColWidth, gap] = [280, 350, 30];
 	let width: number;
 	let height: number;
 	let listView: boolean = false;
+	let listIconName: String = 'list-outline';
+	let gridIconName: String = 'grid-solid';
 	const handleClick = () => {
 		listView = !listView;
+		if (listView) {
+			listIconName = 'rectangle-list-outline';
+			gridIconName = 'grid-outline';
+		} else {
+			listIconName = 'list-outline';
+			gridIconName = 'grid-solid';
+		}
 	};
+	//bind:masonryHeight={height}
+	//bind:masonryWidth={width}
 </script>
 
-<!--Masonry size: <span>{width}px</span> &times; <span>{height}px</span> (w &times;h)-->
-<div class="py-2 px-10 flex flex-row content-center justify-end">
-	<Toggle checked={listView} on:click={handleClick}>List View</Toggle>
+<div class="py-4 px-10 flex flex-row content-center items-center justify-end gap-4">
+	<Icon name={gridIconName} class="h-4 w-4" on:click={handleClick} />
+	<Icon name={listIconName} class="h-6 w-6" on:click={handleClick} />
 </div>
-<div class="flex justify-center">
+<div class="flex justify-center h-fit w-full">
 	{#if !listView}
-		<div class="p-10 md:w-3/5">
+		<div class="p-8 md:w-4/5 -z-10">
 			<Masonry
 				{items}
 				{minColWidth}
 				{maxColWidth}
 				{gap}
 				let:item
-				bind:masonryWidth={width}
-				bind:masonryHeight={height}>
+				bind:masonryHeight={height}
+				bind:masonryWidth={width}>
 				<RecipePreview recipeID={item} />
 			</Masonry>
 		</div>
 	{:else}
-		<div class="p-10 w-3/4 sm:w-3/6 md:w-2/5 lg:4/6 gap-20 flex flex-col justify-center">
+		<div class="p-8 w-3/4 sm:w-3/6 md:w-2/5 lg:4/6 gap-20 flex flex-col justify-center">
 			{#each items as item}
 				<RecipePreview recipeID={item} />
 			{/each}
