e68512d563b20cee2b403bd7cb163df728eb4d18
Zach SChwark <zachs1708@gmail.com>
2023-09-19 23:59:45 +0200

    initial pagenation

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,6 +3,7 @@
 	import RecipePreview from '$lib/components/recipe_preview.svelte';
 	import Masonry from 'svelte-bricks';
 	import { Icon } from 'flowbite-svelte-icons';
+	import IntersectionObserver from 'svelte-intersection-observer';
 
 	export let data: PageData;
 	let items = data.recipes;
@@ -12,6 +13,12 @@
 	let listView: boolean = false;
 	let listIconName: String = 'list-outline';
 	let gridIconName: String = 'grid-solid';
+	let element: any;
+	let intersecting;
+	let pageNumber: number;
+	let tempRecipeList: any;
+	let tempItems;
+
 	const handleClick = () => {
 		listView = !listView;
 		if (listView) {
@@ -22,25 +29,53 @@
 			gridIconName = 'grid-solid';
 		}
 	};
+
+	const getMorePosts = async (pageNum: Number) => {
+		try {
+			const recipeIDs = await fetch(`/?page_num=${pageNum}`, {
+				method: 'GET'
+			});
+
+			const recipe_ids: JSON = await recipeIDs.json();
+			//console.log(recipe_data);
+			return recipe_ids;
+		} catch (recipePreview_err: any) {
+			console.log('error occured in fetch');
+		}
+	};
 </script>
 
 <div class="py-4 px-10 flex flex-row content-center items-center justify-end gap-4">
 	<Icon name={gridIconName} class="h-4 w-4" on:click={handleClick} />
 	<Icon name={listIconName} class="h-6 w-6" on:click={handleClick} />
 </div>
+
 <div class="flex justify-center h-fit w-full">
 	{#if !listView}
 		<div class="p-8 md:w-4/5 -z-10">
-			<Masonry
-				{items}
-				{minColWidth}
-				{maxColWidth}
-				{gap}
-				let:item
-				bind:masonryHeight={height}
-				bind:masonryWidth={width}>
-				<RecipePreview recipeID={item} />
-			</Masonry>
+			<IntersectionObserver
+				{element}
+				on:intersect={async (e) => {
+					console.log('reached the bottom');
+					pageNumber = (pageNumber + 1) * 5;
+					tempRecipeList = await getMorePosts(pageNumber);
+					tempItems = tempRecipeList.recipes;
+					for (let i = 0; i < 5; i++) {
+						items.push(tempItems[i]);
+					}
+				}}>
+				<Masonry
+					bind:this={element}
+					{items}
+					{minColWidth}
+					{maxColWidth}
+					{gap}
+					let:item
+					bind:masonryHeight={height}
+					bind:masonryWidth={width}>
+					<RecipePreview recipeID={item} />
+				</Masonry>
+			</IntersectionObserver>
 		</div>
 	{:else}
 		<div class="p-8 w-3/4 sm:w-3/6 md:w-2/5 lg:4/6 gap-20 flex flex-col justify-center">
---
+++ src/routes/(app)/+server.ts
@@ -0,0 +1,28 @@
+import { turso_client } from '$lib/turso';
+import { json } from '@sveltejs/kit';
+
+export const GET = async ({ url }) => {
+	let pageNum = url.searchParams.get('page_num');
+
+	try {
+		const recipesResult = await turso_client.execute({
+			//sql: 'select name, description, image_url from recipes where recipe_id = ?',
+			sql: 'SELECT recipe_id FROM recipes WHERE recipe_id NOT IN ( SELECT recipe_id FROM recipes ASC LIMIT ? ) LIMIT 5',
+			args: [pageNum]
+		});
+
+		let recipesList: Array<string> = [];
+
+		for (let row of recipesResult.rows) {
+			if (row['recipe_id'] != null) {
+				recipesList.push(row['recipe_id'] as string);
+			}
+		}
+
+		return json({
+			recipes: recipesList
+		});
+	} catch (e) {
+		console.error(e);
+	}
+};
