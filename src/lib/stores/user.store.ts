9eadfe4be617845fb7ee1d521cce8ec237415e87
Orwellian <orwellian225@gmail.com>
2023-09-25 11:04:18 +0200

    add: User store

--- src/lib/stores/user.store.ts
+++ src/lib/stores/user.store.ts
@@ -1,3 +1,12 @@
-import { writable } from 'svelte/store';
+import { writable, type Writable } from 'svelte/store';
+import type { User } from '$lib/types/user.type';
 
-export const user_signed_in = writable(false);
+export const userSignedIn = writable(false);
+export const userInfo: Writable<User> = writable({
+	userId: 'NONE',
+	pronouns: 'NONE',
+	pictureUrl: 'NONE',
+	userName: 'NONE',
+	displayName: 'NONE',
+	biography: 'NONE'
+});
