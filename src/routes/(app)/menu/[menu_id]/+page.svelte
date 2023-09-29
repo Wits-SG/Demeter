791e9adcc493df395bce32381c4ba83984586eac
nihal-ranchod <nihal.ranchod@gmail.com>
2023-09-30 01:29:56 +0200

    Add: Light & Dark mode for button

--- src/routes/(app)/menu/[menu_id]/+page.svelte
+++ src/routes/(app)/menu/[menu_id]/+page.svelte
@@ -43,10 +43,11 @@
 		{/each}
 	</div>
 
-	<div class="flex flex-col justify-center items-center">
+	<div
+		class="flex flex-col border-2 dark:border-emerald-300 border-emerald-300 justify-center items-center p-1">
 		<button
 			on:click={deleteMenu}
-			class="rounded-full border-4 w-40 h-10 border-emerald-700 text-emerald-700 dark:text-emerald-300 text-lg font-semibold gap-1">
+			class="rounded-full border-2 w-40 h-10 border-emerald-700 text-emerald-700 dark:text-emerald-300 text-lg font-semibold">
 			Delete Menu
 		</button>
 	</div>
