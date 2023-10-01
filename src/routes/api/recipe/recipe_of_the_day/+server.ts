81c07416ebf7a10f77eeab12bffd0219f3ff0024
nihal-ranchod <nihal.ranchod@gmail.com>
2023-10-01 13:11:19 +0200

    Add: Backend persistence for ROTD

--- src/routes/api/recipe/recipe_of_the_day/+server.ts
+++ src/routes/api/recipe/recipe_of_the_day/+server.ts
@@ -2,63 +2,61 @@ import { turso_client } from '$lib/turso';
 import { error, json } from '@sveltejs/kit';
 import type { RequestEvent } from './$types';
 
-let selectedRecipeID: any; // Store the selected recipe_id
-
 // Function to select a random recipe from the database
 async function selectRandomRecipe() {
 	try {
-		const recipeResult = await turso_client.execute(
-			'SELECT * FROM recipes ORDER BY RANDOM() LIMIT 1'
+		let recipeResult = await turso_client.execute(
+			'SELECT * FROM recipe_of_the_day ORDER BY date DESC LIMIT 1'
 		);
 
-		if (recipeResult.rows.length > 0) {
-			const randomRecipe = recipeResult.rows[0];
-			return {
-				id: randomRecipe['recipe_id'],
-				name: randomRecipe['name'],
-				description: randomRecipe['description'],
-				servingSize: randomRecipe['serving_size'],
-				cookingTime: randomRecipe['cooking_time'],
-				skillLevelId: randomRecipe['skill_level_id'],
-				imageUrl: randomRecipe['image_url']
-			};
-		} else {
-			throw new Error('No recipes found');
+		const recipeDate = new Date((recipeResult.rows[0]['date'] as number) * 1000).setHours(
+			0,
+			0,
+			0,
+			0
+		);
+		const currentDate = new Date().setHours(0, 0, 0, 0);
+
+		if (recipeDate != currentDate) {
+			turso_client.executeMultiple(
+				"delete from recipe_of_the_day; insert into recipe_of_the_day (recipe_id, date) values ((select recipe_id from recipes order by random() limit 1), unixepoch('now'))"
+			);
+			recipeResult = await turso_client.execute(
+				'SELECT * FROM recipe_of_the_day ORDER BY date DESC LIMIT 1'
+			);
 		}
+		return recipeResult;
 	} catch (error) {
-		throw new Error('Failed to fetch recipe');
+		throw new Error('Failed to fetch date');
 	}
 }
 
 export const GET = async (event: RequestEvent) => {
 	try {
-		if (!selectedRecipeID) {
-			// If no recipe is selected for the day, select one and store it
-			const selectedRecipe = await selectRandomRecipe();
-			selectedRecipeID = selectedRecipe.id;
-			return json({ recipe: selectedRecipe });
-		} else {
-			// Use the previously selected recipe for the day
-			const recipeResult = await turso_client.execute({
-				sql: 'SELECT * FROM recipes WHERE recipe_id = ?',
-				args: [selectedRecipeID]
+		const recipeResult = await selectRandomRecipe();
+
+		// Get the selected recipe_id
+		const recipeID = recipeResult.rows[0]['recipe_id'];
+
+		// Query the database to fetch the selected recipe's details
+		const recipeDetailsResult = await turso_client.execute({
+			sql: 'select name, description, image_url from recipes where recipe_id = ?',
+			args: [recipeID]
+		});
+
+		if (recipeDetailsResult.rows.length > 0) {
+			const selectedRecipe = recipeDetailsResult.rows[0];
+
+			return json({
+				recipe: {
+					id: recipeID,
+					name: selectedRecipe.name,
+					description: selectedRecipe.description,
+					imageUrl: selectedRecipe.image_url
+				}
 			});
-
-			if (recipeResult.rows.length > 0) {
-				const selectedRecipe = {
-					id: recipeResult.rows[0]['recipe_id'],
-					name: recipeResult.rows[0]['name'],
-					description: recipeResult.rows[0]['description'],
-					servingSize: recipeResult.rows[0]['serving_size'],
-					cookingTime: recipeResult.rows[0]['cooking_time'],
-					skillLevelId: recipeResult.rows[0]['skill_level_id'],
-					imageUrl: recipeResult.rows[0]['image_url']
-				};
-
-				return json({ recipe: selectedRecipe });
-			} else {
-				throw error(404, 'Selected recipe not found');
-			}
+		} else {
+			throw error(404, 'No matching recipe found');
 		}
 	} catch (e: any) {
 		throw error(500, 'Failed to fetch recipe');
