eaf44f56073bb85c9cd32d34d37b5784f190785c
Zach Schwark <zachs1708@gmail.com>
2023-10-21 13:47:01 +0200

    display all recipes on user profile

--- src/routes/(app)/profile/[user_id]/+page.svelte
+++ src/routes/(app)/profile/[user_id]/+page.svelte
@@ -3,8 +3,45 @@
 	import type { PageData } from './$types';
 	import { fb_auth } from '$lib/firebase';
 	import { goto } from '$app/navigation';
+	import RecipePreview from '$lib/components/recipe_preview.svelte';
+	//@ts-ignore
+	import Masonry from 'svelte-bricks';
+	import { Icon } from 'flowbite-svelte-icons';
+	import IntersectionObserver from 'svelte-intersection-observer';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
+
+	let itemsList: any[] = [];
+	let items: any[] = new Array(0);
+	//items = [];
+	$: items = itemsList;
+	//itemsList = data.recipes;
+	let [minColWidth, maxColWidth, gap] = [280, 350, 30];
+	let width: number;
+	let height: number;
+	let listView: boolean = false;
+	let listIconName: String = 'list-outline';
+	let gridIconName: String = 'grid-solid';
+	let element: any;
+	let intersecting;
+	let pageNumber: number = 1;
+	let tempRecipeList: any;
+	let tempItems;
+
+	const getMorePosts = async (pageNum: Number) => {
+		try {
+			let strPageNum: string = pageNum.toString();
+			const recipeIDs = await fetch(`/?page_num=${strPageNum}`, {
+				method: 'GET'
+			});
+
+			const recipe_ids: JSON = await recipeIDs.json();
+			return recipe_ids;
+		} catch (recipePreview_err: any) {
+			console.error('Failed to fetch next page');
+		}
+	};
 </script>
 
 <div class="w-full h-full flex flex-col gap-12 p-6">
@@ -53,7 +90,28 @@
 			{/if}
 		</div>
 	</div>
-	<div class="flex flex-row justify-center">
-		<p class="font-light text-lg">POSTS</p>
+	<div class="flex justify-center h-fit w-full">
+		<div class="p-8 md:w-4/5 z-0">
+			<Masonry
+				{items}
+				{minColWidth}
+				{maxColWidth}
+				{gap}
+				let:item
+				bind:masonryHeight={height}
+				bind:masonryWidth={width}>
+				<RecipePreview recipeID={item} />
+			</Masonry>
+			<IntersectionObserver
+				{element}
+				on:intersect={async (e) => {
+					tempRecipeList = await getMorePosts(pageNumber);
+					tempItems = tempRecipeList.recipes;
+					itemsList = [...itemsList, ...tempItems];
+					pageNumber = pageNumber + 5;
+				}}>
+				<div bind:this={element} />
+			</IntersectionObserver>
+		</div>
 	</div>
 </div>
