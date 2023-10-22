da3f085256ea0bb20b9299a5625ddfaf29888aee
Orwellian <orwellian225@gmail.com>
2023-10-22 13:51:14 +0200

    fix: picture turso import

--- src/routes/api/picture_post/preview/+server.ts
+++ src/routes/api/picture_post/preview/+server.ts
@@ -1,11 +1,11 @@
-import { turso_client } from '$lib/turso';
+import { tursoClient } from '$lib/server/turso';
 import { json, error } from '@sveltejs/kit';
 
 export const GET = async ({ url }) => {
 	const postID = url.searchParams.get('post_id');
 
 	try {
-		const picture_res = await turso_client.execute({
+		const picture_res = await tursoClient.execute({
 			sql: 'select picture_id, title, description, url from pictures where post_id = ?',
 			args: [postID]
 		});
