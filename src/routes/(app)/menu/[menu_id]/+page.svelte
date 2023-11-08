91a83889da6764a5b4b3f2697821a392507c37a4
Orwellian <orwellian225@gmail.com>
2023-11-08 16:52:41 +0200

    change: Layout

--- src/routes/(app)/menu/[menu_id]/+page.svelte
+++ src/routes/(app)/menu/[menu_id]/+page.svelte
@@ -7,6 +7,7 @@
 	import DisplayRecipe from '$lib/components/menu/DisplayRecipe.svelte';
 	import { createTooltip, melt } from '@melt-ui/svelte';
 	import { fade } from 'svelte/transition';
+	import { Trash, MoveLeft, X } from 'lucide-svelte';
 
 	const {
 		elements: { trigger, content, arrow },
@@ -71,31 +72,49 @@
 	//need a refresh of data
 </script>
 
-<div class="flex justify-center items-center h-full">
+<div class="w-full h-fit grid grid-rows-1 grid-cols-[minmax(250px,_20%)_1fr] p-2">
+	<div class="w-full h-fit col-start-1 flex flex-col justify-center items-center gap-2">
+		<section class="flex flex-col justify-center items-start gap-2 w-1/2 min-w-[100px]">
+			<a
+				class="w-full items-center justify-start flex flex-row rounded-lg border-2 border-emerald-500 dark:bg-emerald-700 bg-emerald-100 py-1 px-2 dark:hover:bg-emerald-800 hover:bg-emerald-300 text-lg gap-2"
+				href="/collections/menus">
+				<MoveLeft />
+				Back
+			</a>
+
+			<button
+				on:click={deleteMenu}
+				class="text-lg py-1 px-2 w-full rounded-md flex flex-row justify-start items-center hover: dark:text-white text-black hover:bg-red-300 bg-red-400 border-2 border-red-600 gap-2">
+				<Trash />
+				Delete Menu
+			</button>
+		</section>
+	</div>
+
 	<div
-		class="flex flex-col justify-evenly w-full max-w-[95vh] h-full border-4 border-emerald-700 dark:border-emerald-300">
+		class="w-full h-fit overflow-y-auto col-start-2 flex flex-col items-center justify-evenly border-emerald-500 dark:border-emerald-300 border-4 p-2 gap-8 max-w-[70vw] place-self-center">
 		<h1
-			class="flex flex-col text-emerald-700 dark:text-emerald-300 text-6xl text-center font-serif italic font-bold outline-dotted">
+			class="flex flex-col py-2 text-emerald-700 dark:text-emerald-300 text-6xl w-10/12 text-center font-serif italic font-bold border-dashed border-4 border-emerald-500">
 			{data.menu_info.name}
 		</h1>
 
 		<!-- This will contain all sections and recipes -->
-		<div class="flex flex-col justify-center items-center py-5">
+		<div class="flex flex-col justify-center items-center w-9/12">
 			{#each data.menu_info.section as section, i}
 				<h2
-					class="text-4xl text-emerald-800 dark:text-emerald-400 px-5 font-sans italic font-semibold justify-center items-center py-2">
+					class="text-4xl text-emerald-800 dark:text-emerald-400 text-center w-full px-5 font-sans italic font-semibold justify-center items-center py-2 border-b-2 dark:border-sky-200 border-sky-800">
 					{section}
 				</h2>
 				<!-- Displaying recipes under their sections -->
-				<div
-					class="flex flex-col w-full items-start outline-dotted outline-sky-900 outline-4">
+				<div class="flex flex-col w-full items-start">
 					{#each data.recipe[i] as recipe}
 						<div class="flex flex-col w-full">
-							<div class="flex flex-row justify-between w-11/12 min-w-[85vh]">
+							<div class="flex flex-row justify-between">
 								<button class="text-2xl px-10 font-sans font-medium">
 									<DisplayRecipe recipeID={recipe.id} recipeName={recipe.name} />
 								</button>
-								<div class="flex flex-row items-center justify center gap-1">
+								<!-- <a href="/recipe/{recipe.id}" class="text-2xl px-10 font-sans font-medium">{recipe.name}</a> -->
+								<div class="flex flex-row items-center justify-center gap-1">
 									<Icon name="clock-outline" class="h-5 w-5" />
 									<p class="">
 										~{recipe.cookingTime} Mins
@@ -103,7 +122,8 @@
 									<button
 										use:melt={$trigger}
 										on:click={() => deleteRecipe(i, recipe.id)}
-										class="text-md px-5 text-red-600">X</button>
+										class="text-md px-5 text-red-500 flex justify-center items-center"
+										><X /></button>
 									{#if $open}
 										<div
 											use:melt={$content}
@@ -127,18 +147,6 @@
 			<button
 				class=" py-5 justify-start rounded-md text-4xl text-emerald-800 border-emerald-700 dark:text-emerald-400">
 				<CreateSection sectionID={lastSectionIndex + 1} menuId={menuID} /></button>
-		</div>
-		<div
-			class="flex flex-row text-emerald-700 dark:text-emerald-300 justify-center items-center text-xl py-1">
-			<a href="/collections/menus">Back</a>
-		</div>
-
-		<div class="flex flex-col justify-center items-center py-5">
-			<button
-				on:click={deleteMenu}
-				class=" rounded-md border-2 w-40 h-10 border-emerald-700 text-emerald-700 dark:text-emerald-300 text-lg font-semibold">
-				Delete Menu
-			</button>
 		</div>
 	</div>
 </div>
