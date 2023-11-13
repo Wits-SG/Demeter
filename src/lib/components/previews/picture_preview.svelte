3cc08e37005a49fef235b3d354886c75e615a26d
Orwellian <orwellian225@gmail.com>
2023-11-13 21:02:37 +0200

    refactor: Change post previews to use #await

--- src/lib/components/previews/picture_preview.svelte
+++ src/lib/components/previews/picture_preview.svelte
@@ -25,7 +25,7 @@
 	let userId = 'NONE';
 	let userDisplayName = 'NONE';
 
-	onMount(async () => {
+	const updateData = async () => {
 		picturePreviewData = await getPreviewData();
 		pictureID = picturePreviewData.picture.pictureID;
 		pictureTitle = picturePreviewData.picture.name;
@@ -33,34 +33,77 @@
 		pictureImageURL = picturePreviewData.picture.imageURL;
 		userId = picturePreviewData.user.id;
 		userDisplayName = picturePreviewData.user.displayName;
-	});
+	};
 </script>
 
-<!--replace with page path-->
-<a href="/picture/{pictureID}" class="w-full">
-	<div
-		class="w-full h-full rounded-md shadow-md shadow-zinc-600 dark:shadow-zinc-300 overflow-hidden flex flex-col gap-1">
-		<img
-			class="h-full w-full md:h-full md:w-full object-cover border-b-8 border-blue-500"
-			src={pictureImageURL}
-			alt="Dish" />
-
-		<div class="flex flex-col justify-center items-start p-4 gap-2">
-			<section class="h-fit w-full flex justify-between items-center">
-				<a href="/profile/{userId}" class="italic text-sm text-neutral-500"
-					>{userDisplayName}</a>
-				<p class="italic text-sm text-neutral-500">Picture</p>
-			</section>
-
-			<section class="h-fit gap-2 flex flex-col justify-center items-start">
-				<p
-					class="flex flex-row items-center uppercase text-sm text-black dark:text-white font-semibold">
-					{pictureTitle}
-				</p>
-				<p class="line-clamp-2 text-zinc-500 dark:text-zinc-300 text-sm">
-					{pictureDescription}
-				</p>
-			</section>
+<div class="w-full h-full rounded-md shadow-md shadow-zinc-600 dark:shadow-zinc-300">
+	{#await updateData()}
+		<div class="w-full h-full flex flex-col justify-center items-center">
+			<svg
+				width="32"
+				height="32"
+				stroke="#fff"
+				viewBox="0 0 32 32"
+				xmlns="http://www.w3.org/2000/svg">
+				<style>
+					.spinner_V8m1 {
+						transform-origin: center;
+						animation: spinner_zKoa 2s linear infinite;
+					}
+					.spinner_V8m1 circle {
+						stroke-linecap: round;
+						animation: spinner_YpZS 1.5s ease-in-out infinite;
+					}
+					@keyframes spinner_zKoa {
+						100% {
+							transform: rotate(360deg);
+						}
+					}
+					@keyframes spinner_YpZS {
+						0% {
+							stroke-dasharray: 0 150;
+							stroke-dashoffset: 0;
+						}
+						47.5% {
+							stroke-dasharray: 42 150;
+							stroke-dashoffset: -16;
+						}
+						95%,
+						100% {
+							stroke-dasharray: 42 150;
+							stroke-dashoffset: -59;
+						}
+					}
+				</style>
+				<g class="spinner_V8m1">
+					<circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3" />
+				</g>
+			</svg>
 		</div>
-	</div>
-</a>
+	{:then undefined}
+		<a href="/picture/{pictureID}" class="w-full h-full overflow-hidden flex flex-col gap-1">
+			<img
+				class="h-full w-full md:h-full md:w-full object-cover border-b-8 border-blue-500"
+				src={pictureImageURL}
+				alt="Dish" />
+
+			<div class="flex flex-col justify-center items-start p-4 gap-2">
+				<section class="h-fit w-full flex justify-between items-center">
+					<a href="/profile/{userId}" class="italic text-sm text-neutral-500"
+						>{userDisplayName}</a>
+					<p class="italic text-sm text-neutral-500">Picture</p>
+				</section>
+
+				<section class="h-fit gap-2 flex flex-col justify-center items-start">
+					<p
+						class="flex flex-row items-center uppercase text-sm text-black dark:text-white font-semibold">
+						{pictureTitle}
+					</p>
+					<p class="line-clamp-2 text-zinc-500 dark:text-zinc-300 text-sm">
+						{pictureDescription}
+					</p>
+				</section>
+			</div>
+		</a>
+	{/await}
+</div>
--- src/lib/components/previews/recipe_preview.svelte
+++ src/lib/components/previews/recipe_preview.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	export let postID: string;
 	let recipePreviewData: any;
 
@@ -25,7 +23,7 @@
 	let userId = 'NONE';
 	let userDisplayName = 'NONE';
 
-	onMount(async () => {
+	const updateData = async () => {
 		recipePreviewData = await getPreviewData();
 		recipeID = recipePreviewData.recipe.recipeID;
 		dishTitle = recipePreviewData.recipe.name;
@@ -33,33 +31,79 @@
 		dishImageURL = recipePreviewData.recipe.imageURL;
 		userId = recipePreviewData.user.id;
 		userDisplayName = recipePreviewData.user.displayName;
-	});
+	};
 </script>
 
-<a href="/recipe/{recipeID}" class="w-full">
-	<div
-		class="w-full h-full rounded-md shadow-md shadow-zinc-600 dark:shadow-zinc-300 overflow-hidden flex flex-col gap-1">
-		<img
-			class="h-full w-full md:h-full md:w-full object-cover border-b-8 border-red-500"
-			src={dishImageURL}
-			alt="Dish" />
-
-		<div class="flex flex-col justify-center items-start p-4 gap-2">
-			<section class="h-fit w-full flex justify-between items-center">
-				<a href="/profile/{userId}" class="italic text-sm text-neutral-500"
-					>{userDisplayName}</a>
-				<p class="italic text-sm text-neutral-500">Recipe</p>
-			</section>
-
-			<section class="h-fit gap-2 flex flex-col justify-start items-start">
-				<p
-					class="flex flex-row items-center uppercase text-sm text-black dark:text-white font-semibold">
-					{dishTitle}
-				</p>
-				<p class="line-clamp-2 text-zinc-500 dark:text-zinc-300 text-sm">
-					{dishDescription}
-				</p>
-			</section>
+<div class="w-full h-full rounded-md shadow-md shadow-zinc-600 dark:shadow-zinc-300">
+	{#await updateData()}
+		<div class="w-full h-full flex flex-col justify-center items-center">
+			<svg
+				width="32"
+				height="32"
+				stroke="#fff"
+				viewBox="0 0 32 32"
+				xmlns="http://www.w3.org/2000/svg">
+				<style>
+					.spinner_V8m1 {
+						transform-origin: center;
+						animation: spinner_zKoa 2s linear infinite;
+					}
+					.spinner_V8m1 circle {
+						stroke-linecap: round;
+						animation: spinner_YpZS 1.5s ease-in-out infinite;
+					}
+					@keyframes spinner_zKoa {
+						100% {
+							transform: rotate(360deg);
+						}
+					}
+					@keyframes spinner_YpZS {
+						0% {
+							stroke-dasharray: 0 150;
+							stroke-dashoffset: 0;
+						}
+						47.5% {
+							stroke-dasharray: 42 150;
+							stroke-dashoffset: -16;
+						}
+						95%,
+						100% {
+							stroke-dasharray: 42 150;
+							stroke-dashoffset: -59;
+						}
+					}
+				</style>
+				<g class="spinner_V8m1">
+					<circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3" />
+				</g>
+			</svg>
 		</div>
-	</div>
-</a>
+	{:then undefined}
+		<a href="/recipe/{recipeID}" class="w-full h-full overflow-hidden flex flex-col gap-1">
+			<img
+				class="h-full w-full md:h-full md:w-full object-cover border-b-8 border-red-500"
+				src={dishImageURL}
+				alt="Dish" />
+
+			<div class="flex flex-col justify-center items-start p-4 gap-2">
+				<section class="h-fit w-full flex justify-between items-center">
+					<a href="/profile/{userId}" class="italic text-sm text-neutral-500"
+						>{userDisplayName}</a>
+					<p class="italic text-sm text-neutral-500">Recipe</p>
+				</section>
+
+				<section class="h-fit gap-2 flex flex-col justify-start items-start">
+					<p
+						class="flex flex-row items-center uppercase text-sm text-black dark:text-white font-semibold">
+						{dishTitle}
+					</p>
+					<p class="line-clamp-2 text-zinc-500 dark:text-zinc-300 text-sm">
+						{dishDescription}
+					</p>
+				</section>
+			</div>
+		</a>
+	{/await}
+	<!-- Brendan: There should really be error handling here, where if some value doesn't work, the whole thing doesn't just disappear -->
+	<!-- But we take what we can get, this should also hopefully avoid accidently navigating to a recipe without a user id -->
+</div>
