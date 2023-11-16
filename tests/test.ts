66a79a4f85ee748c3287491d0848d96ebf2a93fe
Ashlea <smithashlea@gmail.com>
2023-11-16 17:27:34 +0200

    Navigation for cookbooks and menu

--- tests/test.ts
+++ tests/test.ts
@@ -13,3 +13,38 @@ test('NavBar visible on home - not logged in', async ({ page }) => {
 
 	await expect(page.getByText('Login')).toBeVisible();
 });
+
+// test('Cookbooks page opens from collections', async({page})=>{
+// 	await page.goto('/collections');
+
+// 	await page.getByRole('link', { name: 'Cookbooks' }).click();
+// 	await expect(page.getByText('/collections/cookbooks')).toBeVisible();
+
+// });
+
+// test('Menus  page opens from collections', async({page})=>{
+// 	await page.goto('/collection');
+
+// 	await page.getByRole('link', { name: 'Menus' }).click();
+// 	await expect(page.getByText('/collections/menus')).toBeVisible();
+
+// });
+
+test.describe('collections navigation', () => {
+	test.beforeEach(async ({ page }) => {
+		// Go to the starting url before each test.
+		await page.goto('https://recipio.co.za/collections/');
+	});
+
+	test('cookbook navigation', async ({ page }) => {
+		// Assertions use the expect API.
+		await page.goto('https://recipio.co.za/collections/cookbooks');
+		await expect(page).toHaveURL('https://recipio.co.za/collections/cookbooks');
+	});
+
+	test('menu navigation', async ({ page }) => {
+		// Assertions use the expect API.
+		await page.goto('https://recipio.co.za/collections/menus');
+		await expect(page).toHaveURL('https://recipio.co.za/collections/menus');
+	});
+});
